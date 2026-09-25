4ad576fd5251bf18eb88aff3fcddb71614009da9
mitama <zyrsky888@163.com>
2026-09-25 21:15:30 +0800

    2.1.1: 版本号同步到 gradle.properties 与两个加载器的元数据类

--- src/main/java/damage/engine/DamageEngine.java
+++ src/main/java/damage/engine/DamageEngine.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 
 public class DamageEngine implements ModInitializer {
 	public static final String MOD_ID = "damage-engine";
-	public static final String MOD_VERSION = "2.1.0";
+	public static final String MOD_VERSION = "2.1.1";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
